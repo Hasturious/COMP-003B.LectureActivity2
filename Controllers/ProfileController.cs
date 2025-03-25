3b9ca6d043e42a7d6b3914905cf2eb2186df842f
Jean Bryant Figueroa <jbster559@gmail.com>
2025-03-24 17:30:46 -0700

    Add project files.

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using COMP_003B.LectureActivity2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP_003B.LectureActivity2.Controllers
+{
+    public class ProfileController : Controller
+    {
+        [HttpGet]
+
+        public IActionResult About()
+        {
+            return View();
+        }
+        [HttpGet]
+
+        public IActionResult Hobbies()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COMP_003B.LectureActivity2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
